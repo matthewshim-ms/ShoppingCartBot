329c9e24028acf2a0b2e2dd35ebbdde95e584760
Matt Shim (TEKsystems) <v-shimma@microsoft.com>
2018-01-03 13:36:23 -0800

    added addItem handler

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const McDdata = require('./data/menu.json');
 const shoppingCart = require('./shoppingCart');
 let cards = require('./cards');
 
+
 // for LUIS
 const ai = require('botbuilder-ai');
-
 
 // Create server
 let server = restify.createServer();
@@ -23,14 +23,14 @@ const botFrameworkAdapter = new services.BotFrameworkAdapter({
 server.post('/api/messages', botFrameworkAdapter.listen());
 
 // Create Recognizer + intents
-let recognizer = new builder.RegExpRecognizer();
-recognizer.addIntent('CancelIntent', /(quit|cancel)/i);
-recognizer.addIntent('MenuIntent', /(menu|Menu)/i);
-recognizer.addIntent('ViewBurgers', /(burger|burgers)/i);
-recognizer.addIntent('AddItemIntent', /(add|Add)/i);
-recognizer.addIntent('DeleteIntent', /(delete|remove)/i);
-recognizer.addIntent('CheckoutIntent', /(done|checkout|pay)/i);
-recognizer.addIntent('UpdateItem', /(update)/i); 
+// let recognizer = new builder.RegExpRecognizer();
+// recognizer.addIntent('CancelIntent', /(quit|cancel)/i);
+// recognizer.addIntent('MenuIntent', /(menu|Menu)/i);
+// recognizer.addIntent('ViewBurgers', /(burger|burgers)/i);
+// recognizer.addIntent('AddItemIntent', /(add|Add)/i);
+// recognizer.addIntent('DeleteIntent', /(delete|remove)/i);
+// recognizer.addIntent('CheckoutIntent', /(done|checkout|pay)/i);
+// recognizer.addIntent('UpdateItem', /(update)/i); 
 
 // Initialize bot by passing it adapter
 // - Add a logger to monitor bot.
@@ -58,54 +58,63 @@ let luisRecognizer = initLuisRecognizer();
 
 
 const bot = new builder.Bot(botFrameworkAdapter)
+    .use(luisRecognizer)
     .use(new builder.ConsoleLogger())
     .use(new builder.MemoryStorage())
     .use(new builder.BotStateManager())
-    .use(recognizer)
     .onReceive((context) => {
         if (context.request.type === builder.ActivityTypes.message) {
             return luisRecognizer.recognize(context)
             .then((results) => {
+
                 let luisData = results[0];
                 context.reply(`\nYour input generated the following LUIS results:`);
                 context.reply(`Intent name: ${luisData.name}\n\nScore: ${luisData.score}`);
+
                 luisData.entities.forEach((entity) => {
                     context.reply(`Detected entity: \n\nType: ${entity.type}\n\nValue: ${entity.value}\n\nScore: ${entity.score}`);
                 });
 
+                if(luisData.name == 'CheckMenu'){
+                    context.reply(formatAdaptiveCard(cards.foodMenu));
+                }else if(luisData.name == 'ViewCart'){
+                    
+                }else if(luisData.name == 'AddItem'){
+                    
+                    let quantity;
+                    let itemName; 
 
+                    for(let i = 0; i < luisData.entities.length; i++){
+                        if(luisData.entities[i].type == 'builtin.number'){
+                            quantity = luisData.entities[i].value;
+                            break;
+                        }
+                        else{
+                            quantity = 1; 
+                        }
+                    }
+
+                    // For now, only handle one 'item'
+                    for(let i = 0; i < luisData.entities.length; i++){
+                        if(luisData.entities[i].type == 'foodItem')
+                        itemName = luisData.entities[i].value;
+                    }
+               
+                    console.log("quantity: " + quantity + "\n" + "name: " + itemName);
+
+                }else if(luisData.name == 'CheckOut'){
+
+                }else if(luisData.name == 'Delete'){
+
+                }else if(luisDatan.name == 'UpdateItem'){
+
+                }
+                
             })
             .catch((err) => {
                 context.reply('There was an error connecting to the LUIS API');
                 context.reply(err);
             });
-
-           let count = context.state.conversation.count || 1;
-
-           // handle intents
-        //    if(context.ifIntent('MenuIntent')){
-        //        // TODO: Edit foodMenu.json schema 
-        //        context.reply(formatAdaptiveCard(cards.foodMenu));
-        //    }
-        //    else if(context.ifIntent('ViewBurgers')){
-        //        // TODO: serve adaptive card with just burgers as proof of concept? 
-
-        //    }
-        //    else if(context.ifIntent('CancelIntent')){
-        //        context.reply('Cancelling Order!');
-        //        context.endOfConversation();
-        //    }else if(context.ifIntent('AddItemIntent')){
-                
-
-        //         context.reply('Adding to cart...');
-
-        //    }else if(context.ifIntent('DeleteIntent')){
-        //        context.reply('deleting item from your shopping cart...');
-
-        //    }else if(context.ifIntent('CheckoutIntent')){
-        //         context.reply('Order complete....processing payment...');
-        //         context.endOfConversation();
-        //    }
         } else {
             context.reply(`[${context.request.type} event detected]`);
         }
