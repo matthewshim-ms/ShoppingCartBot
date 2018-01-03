e3596367086d0883cec8fe5b62f948432bf22a36
ted Li <tedlee@microsoft.com>
2018-01-03 12:20:38 -0800

    add ShoppingCart interface

--- shoppingCart.js
+++ shoppingCart.js
@@ -4,9 +4,8 @@ const menu = fs.readFileSync("./data/menu.json");
 
 let menuItems = JSON.parse(menu);
 var menuKeys = Object.keys(menuItems);
-
 module.exports = shoppingCart = {
-    addItem: function(context, item){
+    addItem: function(context, name, count){
         
         // TODO: more error handling,
 
@@ -17,8 +16,29 @@ module.exports = shoppingCart = {
 
         cart.push(cart);
         context.reply(`Added `);
+    },
+
+    deleteItem: function(context, name){
+
+    },
+
+    view: function(context){
+
+    },
+
+    updateItem: function(context, name, count){
+
+    },
+
+    checkout: function(context){
+
     }
+}
 
-
+class ShoppingItem {
+    constructor (name, count) {
+        this.Name = name;
+        this.Count = count;
+    }
 }
 // END 
