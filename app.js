1164aa9ad03f12bff0b1eeb6a88910a24335a656
Matt Shim (TEKsystems) <v-shimma@microsoft.com>
2018-01-02 16:45:01 -0800

    added static intents

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
-const Bot = require('botbuilder-core').Bot;
-const BotFrameworkAdapter = require('botbuilder-services').BotFrameworkAdapter;
+const builder = require('botbuilder-core');
+const services = require('botbuilder-services');
 const restify = require('restify');
+const prompts = require('botbuilder-prompts');
+const McDdata = require('./data/menu.json');
 
 // Create server
 let server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function(){
-    console.log(`${server.name} listening to ${server.url}`);
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create adapter and listen to our servers '/api/messages' route.
-const botFrameworkAdapter = new BotFrameworkAdapter({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_ID
-});
-
+// Create adapter
+const botFrameworkAdapter = new services.BotFrameworkAdapter({ 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD });
 server.post('/api/messages', botFrameworkAdapter.listen());
 
+// Create Recognizer + intents
+let recognizer = new builder.RegExpRecognizer();
+recognizer.addIntent('CancelIntent', /(quit|cancel)/i);
+recognizer.addIntent('MenuIntent', /(menu|Menu)/i);
+recognizer.addIntent('AddItemIntent', /(add|Add)/i); 
+
+
 // Initialize bot by passing it adapter
+// - Add a logger to monitor bot.
+// - Add storage so that we can track conversation & user state.
+// - Add a receiver to process incoming activities.
+const bot = new builder.Bot(botFrameworkAdapter)
+    .use(new builder.ConsoleLogger())
+    .use(new builder.MemoryStorage())
+    .use(new builder.BotStateManager())
+    .use(recognizer)
+    .onReceive((context) => {
+        if (context.request.type === builder.ActivityTypes.message) {
+           let count = context.state.conversation.count || 1;
 
-const bot = new Bot(botFrameworkAdapter);
+           // handle intents
+           if(context.ifIntent('MenuIntent')){
+               
+           }
+           else if(context.ifIntent('CancelIntent')){
+               context.reply('Cancelling Order!');
+               context.endOfConversation();
+           }
 
-// define bot's onReceive message handler...'hello world' for now
+          
 
-bot.onReceive((context) => {
-    context.reply(`Hello World`);
-});
-
+        } else {
+            context.reply(`[${context.request.type} event detected]`);
+        }
+    });
